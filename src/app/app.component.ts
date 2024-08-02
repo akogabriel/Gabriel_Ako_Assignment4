00b3d4d1dc06ac8b2f12b89e699ae0a684b9c25c
gabriel <akog@mymacewan.ca>
2024-08-02 09:56:08 -0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Gabriel_Ako_Assignment4';
+}
